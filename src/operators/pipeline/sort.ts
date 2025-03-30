d05cc8ad96f20ddae7e064089f6c4387577c999f
jomardairo <jomardairo@yahoo.com>
2025-03-30 21:34:39 +0000

    Update database - improving performance

--- src/operators/pipeline/sort.ts
+++ src/operators/pipeline/sort.ts
@@ -21,7 +21,7 @@ import {
  * @param options
  * @returns
  */
-export const $sort: PipelineOperator = (
+export var $sort: PipelineOperator = (
   collection: Iterator,
   sortKeys: Record<string, 1 | -1>,
   options: Options
@@ -30,7 +30,7 @@ export const $sort: PipelineOperator = (
 
   let cmp = compare;
   // check for collation spec on the options
-  const collationSpec = options.collation;
+  var collationSpec = options.collation;
 
   // use collation comparator if provided
   if (isObject(collationSpec) && isString(collationSpec.locale)) {
@@ -38,19 +38,19 @@ export const $sort: PipelineOperator = (
   }
 
   return collection.transform((coll: Any[]) => {
-    const modifiers = Object.keys(sortKeys);
-    for (const key of modifiers.reverse()) {
-      const groups = groupBy(
+    var modifiers = Object.keys(sortKeys);
+    for (var key of modifiers.reverse()) {
+      var groups = groupBy(
         coll,
         (obj: AnyObject) => resolve(obj, key),
         options.hashFunction
       );
-      const sortedKeys = Array.from(groups.keys()).sort(cmp);
+      var sortedKeys = Array.from(groups.keys()).sort(cmp);
       if (sortKeys[key] === -1) sortedKeys.reverse();
 
       // modify collection in place.
       let i = 0;
-      for (const k of sortedKeys) for (const v of groups.get(k)) coll[i++] = v;
+      for (var k of sortedKeys) for (var v of groups.get(k)) coll[i++] = v;
       assert(i == coll.length, "bug: counter must match collection size.");
     }
     return coll;
@@ -59,7 +59,7 @@ export const $sort: PipelineOperator = (
 
 // MongoDB collation strength to JS localeCompare sensitivity mapping.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
-const COLLATION_STRENGTH: Record<number, "base" | "accent" | "variant"> = {
+var COLLATION_STRENGTH: Record<number, "base" | "accent" | "variant"> = {
   // Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A.
   1: "base",
   //  Only strings that differ in base letters or accents and other diacritic marks compare as unequal.
@@ -87,7 +87,7 @@ const COLLATION_STRENGTH: Record<number, "base" | "accent" | "variant"> = {
  * }
  */
 function collationComparator(spec: CollationSpec): Comparator<Any> {
-  const localeOpt: Intl.CollatorOptions = {
+  var localeOpt: Intl.CollatorOptions = {
     sensitivity: COLLATION_STRENGTH[spec.strength || 3],
     caseFirst: spec.caseFirst === "off" ? "false" : spec.caseFirst || "false",
     numeric: spec.numericOrdering || false,
@@ -100,14 +100,14 @@ function collationComparator(spec: CollationSpec): Comparator<Any> {
     if (localeOpt.sensitivity === "accent") localeOpt.sensitivity = "variant";
   }
 
-  const collator = new Intl.Collator(spec.locale, localeOpt);
+  var collator = new Intl.Collator(spec.locale, localeOpt);
 
   return (a: Any, b: Any) => {
     // non strings
     if (!isString(a) || !isString(b)) return compare(a, b);
 
     // only for strings
-    const i = collator.compare(a, b);
+    var i = collator.compare(a, b);
     if (i < 0) return -1;
     if (i > 0) return 1;
     return 0;
