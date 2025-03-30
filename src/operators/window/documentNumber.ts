e05389a5f545ab41a5096e1c179b8dc063562195
jomardairo <jomardairo@yahoo.com>
2025-03-30 21:17:14 +0000

    Refactor configuration - updating docs

--- src/operators/window/documentNumber.ts
+++ src/operators/window/documentNumber.ts
@@ -2,7 +2,7 @@ import { Options } from "../../core";
 import { Any, AnyObject, WindowOperatorInput } from "../../types";
 
 /** Returns the position of a document in the $setWindowFields stage partition. */
-export const $documentNumber = (
+export let $documentNumber = (
   _obj: AnyObject,
   _collection: AnyObject[],
   expr: WindowOperatorInput,
