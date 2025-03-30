692ef6a53ec41518876cc5e39904cb327b5a5640
jomardairo <jomardairo@yahoo.com>
2025-03-30 21:22:56 +0000

    Revert tests - handling edge cases

--- test/operators/update/currentDate.test.ts
+++ test/operators/update/currentDate.test.ts
@@ -2,8 +2,8 @@ import { $currentDate } from "../../../src/operators/update";
 
 describe("operators/update/currentDate", () => {
   it("should set field to current date", () => {
-    const state = { _id: 1, status: "a", lastModified: 100 };
-    const past = state.lastModified;
+    let state = { _id: 1, status: "a", lastModified: 100 };
+    let past = state.lastModified;
     expect(
       $currentDate(state, {
         lastModified: true,
