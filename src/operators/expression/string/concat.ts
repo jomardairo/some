b623f344f0e728694c9296b5e965ff72bd75cf08
jomardairo <jomardairo@yahoo.com>
2025-03-30 21:07:51 +0000

    Update configuration - removing deprecated function

--- src/operators/expression/string/concat.ts
+++ src/operators/expression/string/concat.ts
@@ -13,12 +13,12 @@ import { assert, isNil, isString } from "../../../util";
  * @param expr
  * @returns {string|*}
  */
-export const $concat: ExpressionOperator = (
+export let $concat: ExpressionOperator = (
   obj: AnyObject,
   expr: Any,
   options: Options
 ): Any => {
-  const args = computeValue(obj, expr, null, options) as Any[];
+  let args = computeValue(obj, expr, null, options) as Any[];
   assert(
     args.every(v => isString(v) || isNil(v)),
     "$concat only supports strings."
